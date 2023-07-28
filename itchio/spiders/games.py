34fccd9e5e1d77550c8b0db4c92b0cb6307b9e60
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-27 23:55:13 -0700

    Refactor GameSpider to scrape top-rated games from itch.io
    - Updated the URL in start_requests() method to fetch top-rated games
    - Modified the yield statement in parse() method to include additional fields: platform, input_methods, tags, collection_title, and collection_author
    - Added default values for certain fields using logical OR operator

--- itchio/spiders/games.py
+++ itchio/spiders/games.py
@@ -1,27 +1,31 @@
 from scrapy import Request, Spider
 
-
 class GameSpider(Spider):
     name = "games"
-
+    
     def __init__(self, name=None, **kwargs):
-        super().__init__(name, **kwargs)
+        super().__init__(name, **kwargs) 
         self.pages = int(kwargs.get("pages", 10))
-
+        
     def start_requests(self):
         for i in range(1, self.pages + 1):
-            yield Request(f"https://itch.io/games?page={i}")
-
+            yield Request(f"https://itch.io/games/top-rated?page={i}")
+            
     def parse(self, response):
         for game in response.css("div.game_cell"):
             yield {
-                "title": game.css("a.title::text").get(),
-                "title_url": game.css("a.title::attr(href)").get(),
-                "game_text": game.css("div.game_text::text").get(),
-                "game_genre": game.css("div.game_genre::text").get(),
-                "author": game.css("div.game_author a::text").get(),    
-                "price": game.css("div.price::text").get(),      
-                "synopsis": game.css("div.synopsis::text").get(),    
-                "rating": game.css("div.rating::text").get(),    
-                "reviewers": game.css("div.reviewers::text").get(),            
+                "title": game.css("a.title::text").get() or "",
+                "title_url": game.css("a.title::attr(href)").get() or "",
+                "game_text": game.css("div.game_text::text").get() or "",
+                "game_genre": game.css("div.game_genre::text").get() or "",
+                "author": game.css("div.game_author a::text").get() or "", 
+                "price": game.css("div.price::text").get() or "",
+                "synopsis": game.css("div.synopsis::text").get() or "",
+                "rating": game.css("div.rating::text").get() or "",
+                "reviewers": game.css("div.reviewers::text").get() or 0,
+                "platform": game.css("div.game_platform span::attr(title)").getall() or [],
+                "input_methods": game.css("div.game_platform span::attr(title)").getall() or [],
+                "tags": game.css("div.game_text::text").re(r'#(\w+)') or [],
+                "collection_title": game.css(".collection_title a::text").getall() or [],
+                "collection_author": game.css(".collection_author a::text").getall() or []
             }
