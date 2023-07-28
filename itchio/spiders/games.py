bf086a0f57ceda55b6aba031fb903f293ee15b24
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-28 01:30:34 -0700

    Update number of pages to scrape and modify game URL
    - Increase the number of pages to scrape from 40 to 100.
    - Modify the game URL to fetch top-rated games instead of all games.
    - Extract additional information for each game, including author, star rating, and number of ratings.

--- itchio/spiders/games.py
+++ itchio/spiders/games.py
@@ -5,18 +5,30 @@ class GameSpider(Spider):
 
     def __init__(self, name=None, **kwargs):
         super().__init__(name, **kwargs)
-        self.pages = int(kwargs.get("pages", 40))
-        
+        self.pages = int(kwargs.get("pages", 100))
+
     def start_requests(self):
         for i in range(1, self.pages + 1):
-            yield Request(f"https://itch.io/games?page={i}")
-         
+            yield Request(f"https://itch.io/games/top-rated?page={i}")
+
     def parse(self, response):
         for game in response.css("div.game_cell"):
+            rating = game.css("div.game_rating")
+            star_value = rating.css("div.star_value span.screenreader_only::text").get()
+            star_value = float(star_value.split()[1]) / 5 # Currently out of 5
+            star_value = round(float(star_value), 2)
+            num_ratings = rating.css("span.rating_count::text").get()
+            num_ratings = num_ratings.replace('(', '').replace(',', '')
+            author = game.css("div.game_author a::text").get()
             yield {
-                "title": game.css("a.title::text").get(default=""),        
-                "title_url": game.css("a.title::attr(href)").get(default=""),  
-                "game_text": game.css("div.game_text::text").get(default=""),  
+                "title": game.css("a.title::text").get(default=""),
+                "title_url": game.css("a.title::attr(href)").get(default=""),
+                "author": author,
+                "game_text": game.css("div.game_text::text").get(default=""),
                 "game_genre": game.css("div.game_genre::text").get(default=""),
-                "platform": ','.join(set(game.css("div.game_platform span::attr(title)").getall())),
+                "platform": ",".join(
+                    set(game.css("div.game_platform span::attr(title)").getall())
+                ),
+                "star_rating": star_value,
+                "num_ratings": num_ratings,
             }
