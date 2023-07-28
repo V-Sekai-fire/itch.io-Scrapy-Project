5fa8468a1af597e60114899eec57fdc924538243
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-07-28 00:30:00 -0700

    Refactor game spider to fetch games from all pages
    - Updated the URL in `start_requests` method to fetch games from all pages instead of just top-rated ones.
    - Removed the commented out line for fetching top-rated games.
    - Added a comment with the original URL for reference.

--- itchio/spiders/games.py
+++ itchio/spiders/games.py
@@ -2,15 +2,16 @@ from scrapy import Request, Spider
 
 class GameSpider(Spider):
     name = "games"
-    
+
     def __init__(self, name=None, **kwargs):
         super().__init__(name, **kwargs) 
         self.pages = int(kwargs.get("pages", 10))
         
     def start_requests(self):
         for i in range(1, self.pages + 1):
-            yield Request(f"https://itch.io/games/top-rated?page={i}")
-                
+            yield Request(f"https://itch.io/games?page={i}")
+         
+    # https://itch.io/games/top-rated
     def parse(self, response):
             for game in response.css("div.game_cell"):
                 yield {
@@ -18,9 +19,9 @@ class GameSpider(Spider):
                     "title_url": game.css("a.title::attr(href)").get(),  
                     "game_text": game.css("div.game_text::text").get(),  
                     "game_genre": game.css("div.game_genre::text").get(),         
-                    "synopsis": game.css("div.synopsis::text").get(),    
-                    "rating": game.css("div.rating::text").get(),          
+                    "synopsis": game.css("div.synopsis::text").get(),
                     "platform": ','.join(game.css("div.game_platform span::attr(title)").getall()),
                     "input_methods": ','.join(game.css("div.game_platform span::attr(title)").getall()),      
                     "tags": '|'.join(game.css("div.game_text::text").re(r'#(\w+)'))
             }
+                
